dce04275c9db1088d2947ce244efcd6c74d651b9
Ad Verhoeven <adverhoeven94@hotmail.com>
2013-01-15 19:22:29 +0100

    correcte sessie starter etc.
    als het goed is pakt hij nu wel de html code. of het ifstatement ook
    helemaal werkt zoals het hoort is nog maar de vraag.

--- header.php
+++ header.php
@@ -1,7 +1,9 @@
-<?xml version="1.1" encoding="utf-8" ?>
-<?php session_start(); 
-	$_SESSION[];
+<?php 
+	session_start();
+	$_SESSION['loggedin']=false;
+	$_SESSION['rank']=0;
 ?>
+<?xml version="1.1" encoding="utf-8" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -24,24 +26,34 @@
         <ul>
             <li><a href="index.php">Home</a></li>
             <li class="sep"><a href="agenda.php">Agenda</a></li>
-			<li class="sep"><a href="create_item.php">Create event</a></li>
-			<li class="sep"><a href="approve_item.php">Approve event</a></li>
+			<?php
+				if($_SESSION['loggedin']==true)
+					{
+						echo "<li class='sep'><a href='create_item.php'>Create event</a></li>";
+						echo "<li class='sep'><a href='approve_item.php'>Approve event</a></li>";
+					}
+			?>
         </ul>
     </div>
 
     <div id="login">
-		<form>
-			<input type="button" class="css3button" value="Logout" id="Logout" />
-		</form>
-
-		<form >
-			<input type="text" class="css3text" name="username" id="username" placeholder="username" />
-			<input type="password" class="css3text" name="password" id="password" placeholder="password" />
-			<input type="button" class="css3button" value="Login" />
-			<p>
-				<span class="register">You can register <a href="register.php">here</a>.</span>
-			</p>
-		</form>
+		<?php
+		if($_SESSION['loggedin']==false)
+			{
+			echo "<form>";
+				echo "<input type='text' class='css3text' name='username' id='username' placeholder='username' />";
+				echo "<input type='password' class='css3text' name='password' id='password' placeholder='password' />";
+				echo "<input type='button' class='css3button' value='Login' />";
+				echo "<p><span class='register'>You can register <a href='register.php'>here</a>.</span></p>";
+			echo "</form>";
+			}
+		else
+			{
+			echo "<form>";
+				echo "<input type='submit' class='css3button' value='Logout' />";
+			echo "</form>";
+			}
+		?>
     </div>
 
 </div>
