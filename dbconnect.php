5d5d1c8ac226eb6ea23f4c5f4dbe11179a0c55c0
Kevin Ouwehand <kevmensuper@gmail.com>
2013-01-21 19:42:12 +0100

    Changed the dbconnect file to use the new PDO method. Changed connection variable from  to

--- dbconnect.php
+++ dbconnect.php
@@ -3,5 +3,9 @@
 	 * "config.php"
 	 */
 	include('config.php');
-	$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+	// "Old" method of making a connection:
+	//$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+    /* New method of making a database connection: */
+    $db = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=UTF-8", $dbUser, $dbPass )
 ?>
